aa40e6b982327d7c0020332436bf69c4778fd6c3
Last1k15 <iegora1446@gmail.com>
2026-07-07 17:26:04 +0300

    Add new sections

--- section.py
+++ section.py
@@ -1,9 +1,26 @@
+from math import pi
 # Сечение
 class Section:
-    def __init__(self, area : int, distance1 : int, distance2 : int, inertiaMoment : int = 0):
+    def __init__(self, area : int, distance1 : int, distance2 : int):
         self.area = area
         self.distance1 = distance1
         self.distance2 = distance2
-        self.inertiaMoment = inertiaMoment
     def __repr__(self):
         return f"(area: {self.area}; dist1: {self.distance1}; dist2: {self.distance2}; inertiaMoment: {self.inertiaMoment})"
+
+class RoundSection(Section):
+    def __init__(self, distance1 : int, distance2 : int, outerDiameter : int, innerDiameter : int = 0):
+        area = pi * (outerDiameter - innerDiameter) ** 2 / 4
+        super().__init__(self, area, distance1, distance2)
+        self.outerDiameter = outerDiameter
+        self.innerDiameter = innerDiameter
+        self.inertiaMoment = pi * (outerDiameter - innerDiameter) ** 4 / 32
+        self.resistanceMoment = pi * (outerDiameter - innerDiameter) ** 2 / 16
+
+class SquareSection(Section):
+    def __init__(self, distance1 : int, distance2 : int, sideLength : int):
+        area = sideLength ** 2
+        super().__init__(area, distance1, distance2, inertiaMoment)
+        self.sideLength = sideLength
+        self.inertiaMoment = (sideLength ** 4) / 12
+        self.resistanceMoment = (sideLength ** 3) / 6
